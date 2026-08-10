d1381d017ac6bce8d39982178c0886b3c2226009
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-10 13:02:21 +0530

    rfr-1.3

--- src/model/fm.h
+++ src/model/fm.h
@@ -2,26 +2,32 @@
 #define FM_H
 
 #include <QObject>
-#include <QtMath>
 
 class FuelDispenserModel : public QObject
 {
     Q_OBJECT
-    
+
     Q_PROPERTY(double currentVolume READ currentVolume WRITE setCurrentVolume NOTIFY currentVolumeChanged)
     Q_PROPERTY(double totalCost READ totalCost WRITE setTotalCost NOTIFY totalCostChanged)
     Q_PROPERTY(double pricePerLiter READ pricePerLiter WRITE setPricePerLiter NOTIFY pricePerLiterChanged)
     Q_PROPERTY(int selectedGrade READ selectedGrade WRITE setSelectedGrade NOTIFY selectedGradeChanged)
     Q_PROPERTY(double presetAmount READ presetAmount WRITE setPresetAmount NOTIFY presetAmountChanged)
     Q_PROPERTY(double targetVolume READ targetVolume WRITE setTargetVolume NOTIFY targetVolumeChanged)
-    
+
     Q_PROPERTY(int simulatedFuelIndex READ simulatedFuelIndex WRITE setSimulatedFuelIndex NOTIFY simulatedFuelIndexChanged)
     Q_PROPERTY(int simulatedKeypadIndex READ simulatedKeypadIndex WRITE setSimulatedKeypadIndex NOTIFY simulatedKeypadIndexChanged)
 
 public:
     explicit FuelDispenserModel(QObject *parent = nullptr);
 
-    enum FuelGrade { None = -1, Petrol = 0, E20, E80, Premium, Diesel };
+    enum FuelGrade {
+        None = -1,
+        Petrol = 0,
+        E20,
+        E80,
+        Premium,
+        Diesel
+    };
     Q_ENUM(FuelGrade)
 
     double currentVolume() const { return m_currentVolume; }
@@ -53,13 +59,14 @@ signals:
     void simulatedKeypadIndexChanged();
 
 private:
-    double m_currentVolume = 0.0;
-    double m_totalCost = 0.0;
-    double m_pricePerLiter = 0.0;
-    int m_selectedGrade = -1;
-    double m_presetAmount = 0.0;
-    double m_targetVolume = 0.0;
-    int m_simulatedFuelIndex = -1; 
-    int m_simulatedKeypadIndex = -1;
+    double m_currentVolume{0.0};
+    double m_totalCost{0.0};
+    double m_pricePerLiter{0.0};
+    int m_selectedGrade{-1};
+    double m_presetAmount{0.0};
+    double m_targetVolume{0.0};
+    int m_simulatedFuelIndex{-1};
+    int m_simulatedKeypadIndex{-1};
 };
-#endif
+
+#endif // FM_H
