3b3816c032c91d7330c9c238b0ef8e99766f3f23
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-10 12:58:49 +0530

    rfr-1

--- src/controller/dc.h
+++ src/controller/dc.h
@@ -1,29 +1,39 @@
 #ifndef DC_H
 #define DC_H
+
 #include <QObject>
 #include <QTimer>
 #include <QElapsedTimer>
 #include <functional>
 #include "../model/fueldispensermodel.h"
+
 class DispenserController : public QObject
 {
     Q_OBJECT
+
 public:
     explicit DispenserController(QObject *parent = nullptr);
-    void setModel(FuelDispenserModel* model);
+
+    void setModel(FuelDispenserModel *model);
+
     Q_INVOKABLE void startAutomatedDemo();
     Q_INVOKABLE void startDispensing();
+
 private slots:
     void onSimulationTick();
+
 private:
-    FuelDispenserModel* m_model = nullptr;
-    QTimer* m_timer = nullptr;
-    QElapsedTimer m_elapsedTimer;
-    double m_flowRate = 0.8;
-    bool m_loop = false;
-    int m_generation = 0; 
-    enum State { Idle, Dispensing, Complete };
-    State m_state = Idle;
-    void simulateKeyPress(int index, int durationMs, std::function<void()> action);
+    FuelDispenserModel *m_model{nullptr};
+    QTimer *m_timer{nullptr};
+    QElapsedTimer m_elapsedTimer{};
+    double m_flowRate{0.8};
+    bool m_loop{false};
+    int m_generation{0};
+
+    enum class State { Idle, Dispensing, Complete };
+    State m_state{State::Idle};
+
+    void simulateKeyPress(int index, int durationMs, const std::function<void()> &action);
 };
-#endif
+
+#endif // DC_H
